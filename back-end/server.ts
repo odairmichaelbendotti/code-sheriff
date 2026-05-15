5599a85a1f02cce73cb298db0adffec4dc604aae
Odair Michael Bendotti <obendotti@gmail.com>
2026-05-15 10:13:17 -0300

    feat: implement better-auth in back-end

--- back-end/server.ts
+++ back-end/server.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import { userRouter } from "./src/routes/user.routes.js";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth.js";
 
 const app = express();
 
+app.all("/api/auth/*", toNodeHandler(auth));
+app.use(express.json());
 app.use("/api", userRouter);
 
 app.listen(4000, () => {
